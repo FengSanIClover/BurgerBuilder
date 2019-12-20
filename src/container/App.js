48aeaf35f73bc460b1db5af9d95248902f9d60db
CloverFeng <sunkesi02@gmail.com>
2019-12-20 21:51:16 +0800

    使用 css module

---
+++ src/container/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from '../assets/styles/core/App.module.css';
+
+function App() {
+  return (
+    <div className={classes.App}>
+      123
+    </div>
+  );
+}
+
+export default App;

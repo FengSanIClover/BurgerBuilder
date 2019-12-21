a901b51b17f92d406706c0967d4a0fab58dfcdc0
CloverFeng <sunkesi02@gmail.com>
2019-12-21 15:14:23 +0800

    159 建立漢堡

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,29 @@ import classes from "../../assets/styles/components/burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients) // ex. ['meat','cheese','bacon','salad']
+        .map((igKey) => {
+            // 將 ingredients 的物件屬性名取出變成陣列
+            return [...Array(props.ingredients[igKey])]  //  array length ex. [1] [0] [2] [1]
+                .map((_, index) => {
+                    // 透過物件屬性名 將值轉為陣列長度
+                    // 僅取得個原料的陣列長度，產生元素，不在乎值，因此用 _ 顯示
+                    return <BurgerIngredient key={igKey + index} type={igKey} />
+                })
+        })
+
+    // console.log(props.ingredients)
+
+    // console.log(transformedIngredients)
+
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {
+                transformedIngredients
+            }
             <BurgerIngredient type="bread-bottom" />
         </div>
     )

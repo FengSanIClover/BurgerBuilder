7c82d1005cbd05e68e69537cd5302a31d1876b2c
CloverFeng <sunkesi02@gmail.com>
2019-12-21 16:56:13 +0800

    162 建立多個原料控制元素

--- src/components/Burger/BuilderControls/BuilderControls.jsx
+++ src/components/Burger/BuilderControls/BuilderControls.jsx
@@ -3,9 +3,23 @@ import classes from "../../../assets/styles/components/buildercontrols.module.cs
 import BuildControl from "./BuildControl/BuildControl"
 
 const builderControls = (props) => {
+
+    const controls = [
+        { label: 'Salad', type: 'salad' },
+        { label: 'Meat', type: 'meat' },
+        { label: 'Cheese', type: 'cheese' },
+        { label: 'Bacon', type: 'bacon' }
+    ]
+
+    const builderControls = controls.map(ctr => {
+        return <BuildControl key={ctr.label} label={ctr.label} />
+    })
+
     return (
         <div className={classes.BuilderControls}>
-            <BuildControl />
+            {
+                builderControls
+            }
         </div>
     );
 }

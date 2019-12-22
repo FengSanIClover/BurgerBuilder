a1463e2334e3992095a7526df4d69565c5ad5463
CloverFeng <sunkesi02@gmail.com>
2019-12-22 17:02:50 +0800

    179 SideDrawer 加入 BackDrop 控制

--- src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -2,18 +2,29 @@ import React from "react";
 import Logo from "../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "../../../assets/styles/components/sidedrawer.module.css";
+import Aux from "../../../hoc/Auxiliary";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const sideDrawer = (props) => {
     // do something
+
+    let attachClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachClasses = [classes.SideDrawer, classes.Open];
+    }
+
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachClasses.join(" ")}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 }
 

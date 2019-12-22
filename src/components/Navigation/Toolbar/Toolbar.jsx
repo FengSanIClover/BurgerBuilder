6d0c5b30b5c90288999ce86c4500ec69c9a1f64a
CloverFeng <sunkesi02@gmail.com>
2019-12-22 15:46:38 +0800

    175 增加導覽列 component

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import classes from "../../../assets/styles/components/toolbar.module.css";
 import Logo from "../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolBar = (props) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
         <Logo />
         <nav>
-            ....
+            <NavigationItems />
         </nav>
     </header>
 );

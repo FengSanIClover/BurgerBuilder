4160337601a903f0674b91e0f3aa3b442fbf26f8
CloverFeng <sunkesi02@gmail.com>
2019-12-22 17:18:28 +0800

    180 加入 sideDrawerToggle component 進行 sideDrawer 控制

--- src/components/Navigation/Toolbar/Toolbar.jsx
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import classes from "../../../assets/styles/components/toolbar.module.css";
 import Logo from "../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import SideDrawerToggle from "../SideDrawer/SideDrawerToggle/SideDrawerToggle";
 
 const toolBar = (props) => (
     <header className={classes.Toolbar}>
-        <div>MENU</div>
+        <SideDrawerToggle clicked={props.sideDrawerToggleClicked} />
         <div className={classes.Logo}>
             <Logo />
         </div>

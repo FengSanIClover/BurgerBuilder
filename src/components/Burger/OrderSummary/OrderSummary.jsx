4a062d1412d092dc4adef476e9d5502a7a633db7
CloverFeng <sunkesi02@gmail.com>
2019-12-22 21:38:55 +0800

    184 加上生命週期方法，避免不需要的 reRender

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -1,33 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../../hoc/Auxiliary";
 import classes from "../../../assets/styles/components/ordersummary.module.css";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingerdients = Object.keys(props.ingredients)
-        .map(igkey => {
-            return (
-                <li key={igkey}>
-                    <span className={classes.TextTransform}>{igkey}</span>:{props.ingredients[igkey]}
-                </li>
-            )
-        });
+class OrderSummary extends Component {
+    // 使用 function component 就可以了，因為不需要使用到生命週期，此次僅為測試觀看用
+    componentWillUpdate() {
+        console.log(`[OrderSummary] Will Update`)
+    }
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {
-                    ingerdients
-                }
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
-            <p>Continue To Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseCoutinued}>CONTINUE</Button>
-        </Aux>
-    )
+    componentDidUpdate() {
+        console.log(`[OrderSummary] Did Update`)
+    }
+
+    render() {
+        const ingerdients = Object.keys(this.props.ingredients)
+            .map(igkey => {
+                return (
+                    <li key={igkey}>
+                        <span className={classes.TextTransform}>{igkey}</span>:{this.props.ingredients[igkey]}
+                    </li>
+                )
+            });
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {
+                        ingerdients
+                    }
+                </ul>
+                <p><strong>Total Price: {this.props.price}</strong></p>
+                <p>Continue To Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseCoutinued}>CONTINUE</Button>
+            </Aux>
+        )
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.jsx
+++ src/components/UI/Modal/Modal.jsx
@@ -1,25 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import classes from "../../../assets/styles/components/modal.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 import Aux from "../../../hoc/Auxiliary";
 
-const modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed} />
-            <div
-                className={classes.Modal}
-                style={{
-                    transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-                    opacity: props.show ? "1" : "0"
-                }}
-            >
-                {
-                    props.children
-                }
-            </div>
-        </Aux>
-    )
+class Modal extends Component {
+
+    // 因不是畫面上即時顯示的介面，不需要一直重新 Render ，在要顯示時再重新 Render 就好
+    shouldComponentUpdate(nextProp, nextState) {
+        return nextProp.show !== this.props.show
+    }
+
+    componentWillUpdate() {
+        console.log(`[Modal] will Update`)
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+                        opacity: this.props.show ? "1" : "0"
+                    }}
+                >
+                    {
+                        this.props.children
+                    }
+                </div>
+            </Aux>
+        )
+    }
 }
 
-export default modal;
+export default Modal;

90cc7f99384493219cd83fe3a02aaa642832ad32
CloverFeng <sunkesi02@gmail.com>
2019-12-22 14:29:03 +0800

    173 增加 Toolbar component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
 import classes from "../../assets/styles/components/layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
     <Aux>
-        <div>toolbar、sidedrawer、backdrop</div>
+        <Toolbar />
         <main className={classes.content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classes from "../../../assets/styles/components/toolbar.module.css";
+
+const toolBar = (props) => (
+    <header className={classes.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>
+            ....
+        </nav>
+    </header>
+);
+
+
+export default toolBar;

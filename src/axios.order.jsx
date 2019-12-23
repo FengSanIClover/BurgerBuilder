1897a725e03047175937e87b469b3c320acea3e7
CloverFeng <sunkesi02@gmail.com>
2019-12-23 12:06:15 +0800

    211 發送一筆訂單資料，新增到 firebase

--- src/axios.order.jsx
+++ src/axios.order.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://react-project-my-burger.firebaseio.com/"
+    baseURL: "https://react-project-my-burger.firebaseio.com"
 })
 
 export default instance;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -4,6 +4,7 @@ import Burger from "../../components/Burger/Burger";
 import BuilderControls from "../../components/Burger/BuilderControls/BuilderControls"
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios.order";
 
 /** 全域用得常數命名都為大寫 */
 const INGREDIENT_PRICE = {
@@ -115,7 +116,26 @@ class BurgerBuilder extends Component {
 
     /** 確認配料細項 */
     purchaseContinueHandler = () => {
-        alert("Cotinue~");
+        // alert("Cotinue~");
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: new Date().toDateString(),
+                address: {
+                    country: "Taiwan",
+                    street: "Ruiguan Rd",
+                    zipCode: "114"
+                },
+                email: "123@123.com.tw"
+            },
+            deliveryMethod: "fastest"
+        }
+
+        // 使用 firebase 新增資料庫，網址後要加 .json ，否則會出現 CORS 錯誤
+        axios.post("/orders.json", order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
     }
 
     render() {

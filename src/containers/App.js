fd8faa7e603ad28b4ecfa80bd6f43045c7cbfbd7
CloverFeng <sunkesi02@gmail.com>
2019-12-23 16:19:12 +0800

    215 於 withErrorHandler 結束時釋放掉 axios，修改 App 為 class

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import Layout from "../hoc/Layout/Layout";
 import BurgerBuilder from "./BurgerBuilder/BurgerBuilder";
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  // state = {
+  //   show: true
+  // }
+
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({ show: false })
+  //   }, 5000)
+  // }
+  render() {
+    return (
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
+  }
+  // render() {
+  //   return (
+  //     <div>
+  //       <Layout>
+  //         {
+  //           this.state.show ? <BurgerBuilder /> : null
+  //         }
+  //       </Layout>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
--- src/hoc/withErrorHandler/withErrorHandler.jsx
+++ src/hoc/withErrorHandler/withErrorHandler.jsx
@@ -22,15 +22,36 @@ const withErrorHandler = (WrappedComponent, axios) => {
         //     })
         // }
 
+        /** 在 component render 後會執行此方法，如果 render 失敗就不會執行 */
+        // componentDidMount() {
+        //     axios.interceptors.request.use(request => {
+        //         this.setState({ error: null })
+        //         return request;
+        //     })
+
+        //     axios.interceptors.response.use(response => response, error => {
+        //         this.setState({ error: error })
+        //     })
+        // }
+
         componentWillMount() {
-            axios.interceptors.request.use(request => {
+            this.reqInterceptor = axios.interceptors.request.use(request => {
                 this.setState({ error: null })
                 return request;
             })
 
-            axios.interceptors.response.use(response => response, error => {
+            this.resInterceptor = axios.interceptors.response.use(response => response, error => {
                 this.setState({ error: error })
             })
+        }
+
+        /** 釋放掉建立的 axios 個體，避免造成 memory leak ， 如果多個 container 都使用此 hoc ，
+         * 當 container 結束後會遺留多個未使用的 axios
+         */
+        componentWillUnmount() {
+            console.log("[WrappedComponent] will unmount", this.reqInterceptor, this.resInterceptor)
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         errorCloseHandler = () => {
